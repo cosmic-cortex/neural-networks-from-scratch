dd694070812bf091acaeb4156893579085934271
cosmic-cortex <theodore.danka@gmail.com>
2019-03-10 14:54:55 +0100

    class for neural nets added

---
+++ nn/net.py
@@ -0,0 +1,11 @@
+class Net:
+    __slots__ = ['layers', 'loss']
+
+    def __init__(self, layers, loss):
+        self.layers = layers
+        self.loss = loss
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
