e7dd2cd1656d5b070129491643747a887b30994a
cosmic-cortex <theodore.danka@gmail.com>
2019-03-10 08:38:12 +0100

    Linear.gradX added

--- nn/layers.py
+++ nn/layers.py
@@ -1,5 +1,6 @@
-import math
 import numpy as np
+
+from math import sqrt
 from abc import ABC, abstractmethod
 
 
@@ -7,11 +8,13 @@ class Layer(ABC):
     def __init__(self, *args, **kwargs):
         pass
 
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
+    def __call__(self, x):
+        self.input = x
+        self.output = self.forward(x)
+        return self.output
 
     @abstractmethod
-    def forward(self, *args, **kwargs):
+    def forward(self, x):
         pass
 
     @abstractmethod
@@ -41,20 +44,29 @@ class Linear(Layer):
     def init_weights(self, in_dim, out_dim):
         scale = 1 / sqrt(in_dim)
         self.weight = scale * (np.random.rand(in_dim, out_dim) - 0.5)
-        self.bias = scale * (np.random.rand(0, out_dim) - 0.5)
+        self.bias = scale * (np.random.rand(1, out_dim) - 0.5)
 
     def forward(self, x):
         """
         Forward pass for the linear layer.
 
         Args:
-            x: numpy.array of shape (n_batch, in_dim)
+            x: numpy.array of shape (1, in_dim)
 
         Returns:
-            y: numpy.array of shape (n_batch, out_dim)
+            y: numpy.array of shape (1, out_dim)
         """
         return np.dot(x, self.weight) + self.bias
 
+    def backward(self, x, Dy):
+        pass
+
+    def gradX(self, x):
+        return self.weight
+
+    def gradW(self, x):
+        pass
+
 
 if __name__ == '__main__':
-    pass
+    linear = Linear(5, 2)
