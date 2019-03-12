353eb4f7c01f18355282da175539481f5ec298d8
cosmic-cortex <theodore.danka@gmail.com>
2019-03-12 10:20:22 +0100

    tests rewritten for new interfaces

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -14,5 +14,5 @@ class Test(unittest.TestCase):
         for in_dim, out_dim, n_batch in product(range(1, 10), range(1, 10), range(1, 10)):
             linear = Linear(in_dim, out_dim)
             x = np.random.rand(n_batch, in_dim)
-            y = linear(x)
+            y = linear.forward(x)
             self.assertEqual(y.shape, (in_dim, out_dim))
