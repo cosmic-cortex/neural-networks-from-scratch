dc997d60f6e0021a81cb9801e08d40ed8d39ad0f
cosmic-cortex <theodore.danka@gmail.com>
2019-03-12 13:02:23 +0100

    Net interface refactored, loss calculation separated

--- nn/net.py
+++ nn/net.py
@@ -1,23 +1,52 @@
 class Net:
-    __slots__ = ['layers', 'loss']
+    __slots__ = ['layers', 'loss_fn']
 
     def __init__(self, layers, loss):
         self.layers = layers
-        self.loss = loss
+        self.loss_fn = loss
 
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
 
-    def forward(self, x, y):
-        # propagating through layers
+    def forward(self, x):
+        """
+        Calculates the forward pass by propagating the input through the
+        layers.
+
+        Args:
+            x: numpy.ndarray. Input of the net.
+
+        Returns:
+            output: numpy.ndarray. Output of the net.
+        """
         for layer in self.layers:
             x = layer(x)
-        # calculating the loss
-        x = self.loss(x, y)
         return x
 
+    def loss(self, x, y):
+        """
+        Calculates the loss of the forward pass output with respect to y.
+        Should be called after forward pass.
+
+        Args:
+            x: numpy.ndarray. Output of the forward pass.
+            y: numpy.ndarray. Ground truth.
+
+        Returns:
+            loss: numpy.float. Loss value.
+        """
+        loss = self.loss_fn(x, y)
+        return loss
+
     def backward(self):
-        dy = self.loss.backward()
+        """
+        Complete backward pass for the net. Should be called after the forward
+        pass and the loss are calculated.
+
+        Returns:
+            d: numpy.ndarray of shape matching the input during forward pass.
+        """
+        d = self.loss_fn.backward()
         for layer in reversed(self.layers):
-            dy = layer.backward(dy)
-        return dy
+            d = layer.backward(d)
+        return d
