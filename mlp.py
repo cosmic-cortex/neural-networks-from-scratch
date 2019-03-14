508806060cfae6897f2453582f1ce0e51d9380e6
cosmic-cortex <theodore.danka@gmail.com>
2019-03-14 11:58:40 +0100

    example updated with a more complicated test case

--- mlp.py
+++ mlp.py
@@ -1,18 +1,33 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from nn.layers import *
-from nn.losses import MeanSquareLoss, CrossEntropyLoss
+from nn.losses import CrossEntropyLoss
+from nn.activations import ReLU
 from nn.net import Net
 
 # generating some data
-X = np.concatenate((np.random.multivariate_normal([-1, -1], [[0.1, 0], [0, 0.1]], size=100),
-                    np.random.multivariate_normal([1, 1], [[0.1, 0], [0, 0.1]], size=100)))
-Y_labels = np.array([0]*100 + [1]*100)
+n_class_size = 100
+r = 2
+X1_offset = np.random.rand(n_class_size, 2) - 0.5
+np.sqrt(np.sum(X1_offset**2, axis=1, keepdims=True))
+X1_offset = r * X1_offset/np.sqrt(np.sum(X1_offset**2, axis=1, keepdims=True))
+X1 = np.random.multivariate_normal([0, 0], [[0.1, 0], [0, 0.1]], size=n_class_size) + X1_offset
+X2 = np.random.multivariate_normal([0, 0], [[0.1, 0], [0, 0.1]], size=n_class_size)
 
-net = Net(layers=[Linear(2, 2)],
+X = np.concatenate((X1, X2))
+Y_labels = np.array([0]*n_class_size + [1]*n_class_size)
+
+with plt.style.context('seaborn-white'):
+    plt.figure(figsize=(10, 10))
+    plt.scatter(X1[:, 0], X1[:, 1], c='r')
+    plt.scatter(X2[:, 0], X2[:, 1], c='b')
+    plt.show()
+
+net = Net(layers=[Linear(2, 4), ReLU(), Linear(4, 2)],
           loss=CrossEntropyLoss())
 
-n_epochs = 100
+n_epochs = 1000
 for epoch_idx in range(n_epochs):
     print("Epoch no. %d" % epoch_idx)
     out = net(X)
